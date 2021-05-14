6fa9c1ae1c20e7ad104d8b0a8602b8ae1d0b5b42
Jangmanbo <wkddpdnjs99@gmail.com>
2021-05-15 05:35:02 +0900

    complete read_proc_list, set_schedule func

--- schedule.c
+++ schedule.c
@@ -1,8 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int mtd;
+int num;
+int** info;
+
 // fn: read_proc_list
 // desc: read process file list
 // param
 // 	file_name: process list name
-void read_proc_list(const char* file_name) {}
+void read_proc_list(const char* file_name) {
+	char line[1024];
+	int buf[1024], idx;
+	FILE* pFile = fopen(file_name, "r");
+	if (pFile != NULL && !feof(pFile)) {
+		fgets(line, 1024, pFile);
+		num = atoi(line);
+		idx = 0;
+		while (!feof(pFile)) {
+			fgets(line, 1024, pFile);
+			//printf("%s", line);
+			char* ptr = strtok(line, " ");	// " " 공백 문자를 기준으로 문자열을 자름, 포인터 반환
+			while (ptr != NULL)				// 자른 문자열이 나오지 않을 때까지 반복
+			{
+				//printf("%s\n", ptr);		// 자른 문자열 출력
+				if (ptr[0]!= '\n')
+				{					
+					buf[idx] = atoi(ptr);
+					//printf("ptr:%s info:%d ", ptr, buf[idx]);
+					idx++;
+				}
+				ptr = strtok(NULL, " ");	// 다음 문자열을 잘라서 포인터를 반환
+			}
+		}
+	}
+	int row = idx / 3;
+	info = malloc(sizeof(int*) * row);
+	for (int i = 0; i < row; i++)
+	{
+		info[i]= malloc(sizeof(int*) * 3);
+	}
+	for (int i = 0; i < idx; i++)
+	{
+		info[i / 3][i % 3] = buf[i];
+		//printf("%d\n", info[i / 3][i % 3]);
+	}
+}
 
 // fn: set_schedule
 // desc: set scheduling method
@@ -15,7 +58,26 @@ void read_proc_list(const char* file_name) {}
 //  4. Round Robin : Time Quantum = 2 (Prremptive)
 //
 // return none
-void set_schedule(int method) {}
+void set_schedule(int method) {
+	switch (method)
+	{
+	case 1:
+		printf("%s", "Scheduling method: FCFS (Non-preemptive)\n");
+		break;
+	case 2:
+		printf("%s", "Scheduling method: SJF (Non-preemptive)\n");
+		break;
+	case 3:
+		printf("%s", "Scheduling method: SRTF (Preemptive)\n");
+		break;
+	case 4:
+		printf("%s", "Scheduling method: RR (Preemptive)\n");
+		break;
+	default:
+		break;
+	}
+	mtd = method;
+}
 
 // fn: do_schedule
 // desc: scheduling function called every tick from main
